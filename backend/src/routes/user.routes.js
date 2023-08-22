67edfc88736e862b5413868f33347d5cba1b7080
emilio rivas <emiliorivasruiz@gmail.com>
2023-08-22 09:09:52 -0500

    express-validator

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,13 @@
 const userController = require('../controllers/user.controller');
+const {
+    loginValidation,
+    createUserValidation,
+} = require('../middlewares/validated.middleware');
 const express = require('express');
 
 const router = express.Router();
 
-router.post('/create', userController.createUser);
-router.post('/login', userController.login);
+router.post('/create', createUserValidation, userController.createUser);
+router.post('/login', loginValidation, userController.login);
 
 module.exports = router;
