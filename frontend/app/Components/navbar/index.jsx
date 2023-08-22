f75985e4cb7278b8f2dcd9aecb823c2262f06799
VickyGB <vickygbonorino@gmail.com>
2023-08-22 15:50:24 -0300

    style: navBar web

--- frontend/app/Components/navbar/index.jsx
+++ frontend/app/Components/navbar/index.jsx
@@ -2,15 +2,16 @@ import Image from "next/image"
 
 export  function NavBar(){
     return(
-        <div className="bg-Azul/50 w-9/12 h-20 flex items-center ">
-            <div className="basis-1/2">
-                <Image src="/public/next.svg" width={60} height={60}></Image>
+        <div className="bg-Morado/50 w-full h-24 flex items-center ">
+            <div className="basis-1/3 place-content-center p-12">
+                <Image src="/logos/Logo-CPweb.svg" width={255.64} height={46}  />
             </div>
-            <div className="basis-1/2 space-x-10 text-xs">   
-                <a src="/pages/index.js">Home</a>
-                <a src="/pages/cartera.js">Cartera</a>
-                <a src="/pages/movimientos.js">Movimientos</a>
-                <button className="basis-1/4 bg-gray-900 w-32 h-12 rounded-md py-3.5 px-7"></button>
+            <div className="basis-1/3 space-x-10 text-base place-content-center">   
+                <a src="/pages/index.js">Sobre nosotros</a>
+                <a src="/pages/cartera.js">Características</a>
+            </div>
+            <div className="flex basis-1/3 place-content-center">
+                <button className=" bg-Morado/700 w-32 h-12 rounded-md py-3.5 px-7 flex items-center justify-center text-white ">Registrarme</button>
             </div>
             
         </div>
